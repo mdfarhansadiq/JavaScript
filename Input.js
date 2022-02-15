28ad430161635e6c8b1bb5a007ee6f5224fa50dc
Md. Farhan Sadiq <42261830+mdfarhansadiq@users.noreply.github.com>
2022-02-15 17:48:27 +0600

    Create Input.js

---
+++ Input.js
@@ -0,0 +1,13 @@
+/// *******In the name of Allah*******;
+
+var readline = require('readline');
+
+var rl = readline.createInterface(
+	process.stdin, process.stdout);
+
+rl.question('', (test) => { /// 'test' is just a variable to take input
+    
+  /// write any code here
+  
+    rl.close(); /// close to take input
+});
