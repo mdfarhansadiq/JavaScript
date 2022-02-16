04d0052c5c2f3984c93bbe1f7da5031e1ae49a71
Md. Farhan Sadiq <42261830+mdfarhansadiq@users.noreply.github.com>
2022-02-16 06:48:53 +0600

    Create Input from Internet Browser.js

---
+++ Input from Internet Browser.js
@@ -0,0 +1,15 @@
+<!--/// *******In the name of Allah******* ///-->
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>JavaScript in HTML</title>
+    </head>
+    <body>
+        <script>
+            var val = prompt("Enter the value: ");
+            //document.write(val); /// output on page
+            //console.log(val); /// inspect element on browser. then console option for output
+        </script>
+    </body>
+</html>
