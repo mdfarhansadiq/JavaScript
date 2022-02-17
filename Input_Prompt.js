ef8029044a2c73d5e27a761c971ff75fcf7f71ba
Md. Farhan Sadiq <42261830+mdfarhansadiq@users.noreply.github.com>
2022-02-17 08:07:23 +0600

    Update and rename Input-Prompt.js to Input_Prompt.js

---
+++ Input_Prompt.js
@@ -0,0 +1,7 @@
+/// *******In the name of Allah******* ///
+
+const prompt = require("prompt-sync")();
+
+var val = prompt('Enter The Value: '); /// to take input from console
+
+var a = parseInt(prompt()); /// 'parseInt()' is to convert input into Integer
