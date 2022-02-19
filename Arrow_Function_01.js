3a99d62fcf9f55d83ac5b16724f11505635121c8
Md. Farhan Sadiq <42261830+mdfarhansadiq@users.noreply.github.com>
2022-02-19 19:36:01 +0600

    Create Arrow_Function_01.js

---
+++ Arrow_Function_01.js
@@ -0,0 +1,7 @@
+/// *******In the name of Allah*******;
+
+var squareNum = (num) => { /// "Arrow Function" does not need "function" keyword to be declared as function
+    return num * num;
+};
+
+console.log(squareNum(786));
