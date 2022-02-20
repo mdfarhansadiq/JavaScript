100c6a7e1503c053460ce76b0d93aee5f14c2bdc
Md. Farhan Sadiq <42261830+mdfarhansadiq@users.noreply.github.com>
2022-02-20 07:13:16 +0600

    Update Arrow_Function_02.js

--- Arrow_Function_02.js
+++ Arrow_Function_02.js
@@ -1,5 +1,5 @@
 /// *******In the name of Allah*******;
 
-var squareNum = (num) => (num * num) /// "Arrow Function" also does not need "return" keyword to return value
+var squareNum = (num) => (num * num) /// "Arrow Function" also does not need "return" keyword to return value if there is no conditional statement
 
 console.log(squareNum(786));
