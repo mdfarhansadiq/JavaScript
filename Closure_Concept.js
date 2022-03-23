41215326c0921cee3fc8e1098d731ab15a25a8f5
Md. Farhan Sadiq <42261830+mdfarhansadiq@users.noreply.github.com>
2022-03-23 23:16:13 +0600

    Update Closure_Concept.js

--- Closure_Concept.js
+++ Closure_Concept.js
@@ -13,6 +13,7 @@ function closure01(x)
     return closure02;
 }
 
+
 var a = closure01(17) /// closure01 returns closure 02
 var b = a(27) /// closure02 returns closure03
 var c = b(37) /// finally closure03 returns the answer;
