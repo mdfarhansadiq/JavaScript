f2bb3daa50ad1a21f768546699cd2eb4abc194ea
Md. Farhan Sadiq <42261830+mdfarhansadiq@users.noreply.github.com>
2022-03-30 22:53:57 +0600

    Create CallBack02.js

---
+++ CallBack02.js
@@ -0,0 +1,13 @@
+/// *******In the name of Allah******* ///
+
+function Print(ans)
+{
+    console.log(ans);
+}
+function func(num1, num2, num3, callback)
+{
+    if(callback)
+        callback((num1 + num2 + num3));
+}
+
+func(7, 77, 777, Print);
