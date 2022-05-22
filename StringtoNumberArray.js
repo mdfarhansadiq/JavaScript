02f314876568eff6165b0dff9aa083a29f952a8c
Md. Farhan Sadiq <42261830+mdfarhansadiq@users.noreply.github.com>
2022-05-22 14:54:57 +0600

    Create StringtoNumberArray.js

---
+++ StringtoNumberArray.js
@@ -0,0 +1,14 @@
+/// *******In the name of Allah******* ///
+
+var num = "56 65 74 100 99 68 86 180 90";
+num += " ";
+var Mn = 9999999;
+var a = "", b = 0, c = [];
+for(var i = 0;i<num.length;i++)
+{
+    if(num[i]===" ")
+        b = parseInt(a), c.push(b), a = "";
+    else
+        a += num[i];
+}
+console.log(c);
